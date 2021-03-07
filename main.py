be1bf9948c214845c644e77c13a37920a6b09aa4
Sergey Tissen <earnedgravy@gmail.com>
2021-03-08 00:23:58 +0700

    Add new methods and output results for specific objects

--- main.py
+++ main.py
@@ -14,6 +14,30 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        avg_list = []
+        for grade in self.grades.values():
+            avg_list += grade
+        return avg_list
+
+    def __str__(self):
+        return ('Студент' + '\n' + 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname + '\n' +
+                'Средняя оценка за домашние задания: ' +
+                str(sum(self.average_grade()) / len(self.average_grade())) + '\n' + 'Курсы в процессе изучения: ' +
+                ', '.join(self.courses_in_progress) + '\n' + 'Завершённые курсы: ' +
+                ', '.join(self.finished_courses) + '\n')
+
+    def __gt__(self, other):
+        if not isinstance(other, Student):
+            return 'Ошибка! Это не студент!'
+        else:
+            if self.average_grade() > other.average_grade():
+                return ('Студент ' + self.name + ' ' + self.surname + ' успешнее, чем ' +
+                        other.name + ' ' + other.surname + '\n')
+            else:
+                return ('Студент ' + other.name + ' ' + other.surname + ' успешнее, чем ' +
+                        self.name + ' ' + self.surname + '\n')
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -27,6 +51,25 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.rating = []
 
+    def average_rate(self):
+        avg_rate = sum(self.rating) / len(self.rating)
+        return avg_rate
+
+    def __str__(self):
+        return ('Лектор' + '\n' + 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname + '\n' +
+                'Средняя оценка за лекции: ' + str(self.average_rate()) + '\n')
+
+    def __gt__(self, other):
+        if not isinstance(other, Lecturer):
+            return 'Ошибка! Это не лектор!'
+        else:
+            if self.average_rate() > other.average_rate():
+                return ('Лектор ' + self.name + ' ' + self.surname + ' успешнее, чем ' +
+                        other.name + ' ' + other.surname + '\n')
+            else:
+                return ('Лектор ' + other.name + ' ' + other.surname + ' успешнее, чем ' +
+                        self.name + ' ' + self.surname + '\n')
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -38,25 +81,44 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']
-
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+    def __str__(self):
+        return 'Проверяющий' + '\n' + 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname + '\n'
 
 
-print(best_student.grades)
+student_ivanov = Student('Иванов', 'Иван', 'male')
+student_ivanov.courses_in_progress += ['Python', 'Git']
 
-cool_lecturer = Lecturer("Some", "Buddy")
-cool_lecturer.courses_attached += ['Python']
+student_petrova = Student('Петрова', 'Ольга', 'female')
+student_petrova.courses_in_progress += ['Python']
+student_petrova.finished_courses += ['Git']
 
-best_student.rate_lecturer(cool_lecturer, 'Python', 10)
-best_student.rate_lecturer(cool_lecturer, 'Python', 10)
-best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+reviewer_sidorova = Reviewer('Анна', 'Сидорова')
+reviewer_sidorova.courses_attached += ['Python']
 
-print(cool_lecturer.rating)
+reviewer_andropov = Reviewer('Анатолий', 'Андропов')
+reviewer_andropov.courses_attached += ['Git']
+
+lecturer_ignatenko = Lecturer('Марина', 'Игнатенко')
+lecturer_ignatenko.courses_attached += ['Python']
+
+lecturer_andropov = Lecturer('Анатолий', 'Андропов')
+lecturer_andropov.courses_attached += ['Git']
+
+reviewer_sidorova.rate_hw(student_ivanov, 'Python', 7)
+reviewer_andropov.rate_hw(student_ivanov, 'Git', 8)
+
+reviewer_sidorova.rate_hw(student_petrova, 'Python', 9)
+
+student_ivanov.rate_lecturer(lecturer_ignatenko, 'Python', 8)
+student_ivanov.rate_lecturer(lecturer_andropov, 'Git', 6)
+
+
+print(student_ivanov)
+print(student_petrova)
+print(reviewer_sidorova)
+print(reviewer_andropov)
+print(lecturer_ignatenko)
+print(lecturer_andropov)
+
+print(lecturer_ignatenko > lecturer_andropov)
+print(student_ivanov > student_petrova)
