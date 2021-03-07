37707a6c81bb425613f5808dd8a7b7cea802c275
Sergey Tissen <earnedgravy@gmail.com>
2021-03-07 18:13:31 +0700

    Add new classes for lecturers and reviewers, move grade method from mentor to reviewer, add new method for rate lecturer by student

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, rate):
+        if (isinstance(lecturer, Lecturer) and course in self.courses_in_progress and
+                course in lecturer.courses_attached):
+            lecturer.rating += [rate]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +21,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.rating = []
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -27,11 +42,21 @@ class Mentor:
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
 
 print(best_student.grades)
+
+cool_lecturer = Lecturer("Some", "Buddy")
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+
+print(cool_lecturer.rating)
